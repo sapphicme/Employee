18098d7393d93dda36666f569283d0c75fd3dae7
Arielle Mueller <43253070+sapphicme@users.noreply.github.com>
2019-02-07 15:31:12 -0500

    Add files via upload

---
+++ Assignment2_PROG2/Employee.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2_PROG2
+{
+    class Employee
+    {
+        static void Main(string[] args)
+        {
+
+
+        }
+
+        readonly public int EmployeeId;
+        public string FirstName;
+        public string LastName;
+        public double BaseSalary = 1000.00;
+        public double GrossSales;
+        public double ComissionRate = 0.1;
+
+
+        public void Earnings(double comissionCalc)
+        {
+            comissionCalc = ComissionRate * GrossSales + BaseSalary;
+            return;
+        }
+
+        public void Employee()
+        {
+            
+        }
+
+        public Employee(int employeeID, string firstName, double baseSalary)
+        {
+            EmployeeId = employeeID;
+            FirstName = firstName;
+            BaseSalary = baseSalary;
+
+        }
+    }
+}
